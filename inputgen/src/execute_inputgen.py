92bae17f9ac95159acc31e7a51e5377fd01efe20
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2025-08-13 17:19:03 +0000

    Add a workflow module that produces coordinating input files

---
+++ inputgen/src/execute_inputgen.py
@@ -0,0 +1,97 @@
+"""This is a script to generate the data needed to coordinate the execution of 
+the MUR algorithms.
+"""
+
+# Standard
+import argparse
+import datetime
+import json
+import logging
+import pathlib
+
+
+# Constants
+L2P_JSON = "l2p.json"
+
+
+# Logger
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.INFO
+)
+
+
+def create_args():
+    """Create and return argparser with arguments."""
+
+    arg_parser = argparse.ArgumentParser(description="Execute Land Ice operations for MUR input")
+    arg_parser.add_argument("-o",
+                            "--output",
+                            type=pathlib.Path,
+                            help="Full path to directory to save results")
+    arg_parser.add_argument("-c",
+                            "--config",
+                            type=str,
+                            help="Full path to configuration file")
+    return arg_parser
+
+
+def generate_l2p(config_file, output_dir):
+    """Generate L2P coordinating JSON file."""
+
+    sensor_dict = get_config_data(config_file)
+
+    coord_json = {}
+    for sensor, data in sensor_dict.items():
+        # Get date range to retrieve sensor data for
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=data["day_range"][0])
+        coord_json[sensor] = list(get_date_range(start_date, end_date))
+
+    out_file = output_dir.joinpath(L2P_JSON)
+    with open(out_file, "w") as jf:
+        json.dump(coord_json, jf, indent=2)
+
+    logging.info("Wrote L2P JSON: %s", out_file)
+
+
+def get_config_data(config_file):
+    """Retrieve configuration data to execute L2P operations."""
+
+    if "s3" in config_file:
+        with fsspec.open(config_file, mode='r') as fh:
+            config_data = json.load(fh)
+    else:
+        with open(config_file) as fh:
+            config_data = json.load(fh)
+    return config_data
+
+
+def get_date_range(start_date, end_date):
+    """Generate a sequence of dates from the start date to the end date."""
+
+    for d in range(int((end_date - start_date).days) + 1):
+        day = start_date + datetime.timedelta(d)
+        doy = day.timetuple().tm_yday
+        yield (doy, day.year)
+
+
+def main():
+    """Main script to generate coordinating JSON files."""
+
+    # Command line args
+    arg_parser = create_args()
+    args = arg_parser.parse_args()
+    output_dir = args.output
+    config = args.config
+    for name, value in vars(args).items(): logging.info("%s: %s", name, value)
+
+    # L2P
+    logging.info("Generating L2P sensor coordinating JSON file.")
+    generate_l2p(config, output_dir)
+
+
+if __name__ == "__main__":
+    main()
